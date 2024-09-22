0a419d045986d9636bff96db14234e4e433bd4d8
m-maqshm <160467214+m-maqshm@users.noreply.github.com>
2024-09-22 12:27:28 +0300

    add asset foder

---
+++ lib/helpers/storage_helper.dart
@@ -0,0 +1,27 @@
+import 'package:get_storage/get_storage.dart';
+
+class StorageHelper{
+  StorageHelper._();
+  static StorageHelper? sHelper;
+
+  static StorageHelper get instance{
+    if(sHelper==null)
+      sHelper=StorageHelper._();
+    return sHelper!;
+  }
+
+  GetStorage box=GetStorage();
+
+  readKey(String key){
+  return box.read(key);
+  }
+  writeKey(String key,String value){
+box.write(key, value);
+  }
+  removeKey(String key){
+    box.remove(key);
+  }
+  removeAll(){
+    box.erase();
+  }
+}
