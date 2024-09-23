8411c6e9ead617beb38ffe00bcb2cc0070c75df1
logmanbashreef <164614416+logmanbashreef@users.noreply.github.com>
2024-09-23 10:17:48 +0300

    update bottone

--- lib/core/views/screens/signup_screen.dart
+++ lib/core/views/screens/signup_screen.dart
@@ -117,7 +117,7 @@ class SignupScreen extends StatelessWidget {
                       Row(
                         children: [
                           const Padding(
-                            padding: EdgeInsets.only(left: 35, bottom: 7),
+                            padding: EdgeInsets.only(left: 35, bottom: 8),
                             child: Text(
                               "Gender",
                               style: TextStyle(
